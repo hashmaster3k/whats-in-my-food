5fbf980720d706d3c882a6d03ea2a2bd8ccdf84a
Hashim Gari <hashmaster3k@gmail.com>
2020-10-19 09:37:32 -0600

    Setup for displaying items

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'FOODS INDEX PAGE' do
-  it 'fills form, submit and hits correct path' do
+  before :each do
     visit root_path
 
-    fill_in :q, with: 'test'
+    fill_in :q, with: 'sweet potatoes'
     click_button 'Search'
+  end
 
+  it 'fills form, submit and hits correct path' do
     expect(current_path).to eq('/foods')
+  end
+
+  it 'returns 10 results' do
+    expect(page).to have_content('Results returned: 10')
+    expect(page).to have_css('.food', count: 10)
   end
 end
