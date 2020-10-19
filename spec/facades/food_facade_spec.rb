0734772b5bce4dbb4a65b35248596b8ad9db3af8
Hashim Gari <hashmaster3k@gmail.com>
2020-10-19 10:13:01 -0600

    Update Facade test to ensure for proper output

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe 'Food Facade' do
   it 'returns an array of foods' do
     foods = FoodFacade.get_foods_with_ingredient('beef')
     expect(foods).to be_an(Array)
+    expect(foods.first).to be_an(Food)
+    expect(foods.first.gtin_upc).to be_a(String)
+    expect(foods.first.description).to be_a(String)
+    expect(foods.first.brand_owner).to be_a(String)
+    expect(foods.first.ingredients).to be_a(String)
   end
 end
